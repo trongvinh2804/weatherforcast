a7aec68d86ad514141715ae6f2aca41867e65ae8
trongvinh2804 <trongvinh28042000@gmail.com>
2025-03-26 13:21:52 +0700

    init

--- app/src/main/java/com/example/weatherforecast2025/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast2025/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.weatherforecast2025
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherforecast2025.MyListViewAdapter.MyListViewAdapter
@@ -9,6 +13,7 @@ import com.example.weatherforecast2025.models.Weather
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: WeatherLayoutBinding
+    private lateinit var city: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = WeatherLayoutBinding.inflate(layoutInflater)
@@ -16,7 +21,27 @@ class MainActivity : AppCompatActivity() {
 
         val arrSpinner = arrayListOf("HaNoi", "HoChiMinh")
         val adapterSpinner = ArrayAdapter(this,android.R.layout.simple_spinner_item,arrSpinner)
+
+        // bắt sự kiện cho
+        binding.spKhuVuc.onItemSelectedListener = object : OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                // lấy tên của item người dùng chọn trong
+                city = parent?.selectedItem.toString()
+                Log.d("test",city)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+        }
+
         binding.spKhuVuc.adapter = adapterSpinner
+
+
         val arrWeathers = arrayListOf(
             Weather(1,"20 phut",30f, 32f,28f,"Clear",68f),
             Weather(1,"20 phut",30f, 32f,28f,"Cloud",68f)
