02f99e1ee42f8e1715c24296db61cef848d6bfe7
trongvinh2804 <trongvinh28042000@gmail.com>
2025-03-26 13:55:38 +0700

    init

--- app/src/main/java/com/example/weatherforecast2025/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast2025/MainActivity.kt
@@ -13,13 +13,15 @@ import com.example.weatherforecast2025.models.Weather
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: WeatherLayoutBinding
+    private lateinit var adapter: MyListViewAdapter
+    private lateinit var arrWeathers : ArrayList<Weather>
     private lateinit var city: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = WeatherLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val arrSpinner = arrayListOf("HaNoi", "HoChiMinh")
+        val arrSpinner = arrayListOf("HaNoi", "HoChiMinh","DaNang", "Hue", "CanTho")
         val adapterSpinner = ArrayAdapter(this,android.R.layout.simple_spinner_item,arrSpinner)
 
         // bắt sự kiện cho
@@ -42,11 +44,11 @@ class MainActivity : AppCompatActivity() {
         binding.spKhuVuc.adapter = adapterSpinner
 
 
-        val arrWeathers = arrayListOf(
+         arrWeathers = arrayListOf(
             Weather(1,"20 phut",30f, 32f,28f,"Clear",68f),
             Weather(1,"20 phut",30f, 32f,28f,"Cloud",68f)
         )
-        var adapter = MyListViewAdapter(this,arrWeathers)
+        adapter = MyListViewAdapter(this,arrWeathers)
         binding.lvKetQua.adapter = adapter
     }
 
